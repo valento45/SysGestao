7ef21803a8a7043771a1b9e0b0ed2c5ebca9da47
valento45 <iggor1935@hotmail.com>
2022-01-19 08:13:06 -0300

    ajuste banco de dados

--- bdsysgestai.sql
+++ bdsysgestai.sql
@@ -14,8 +14,29 @@ CREATE DATABASE sysgestao
 	quantidade integer not null default 0,
 	variacao varchar(100) not null,
     descricao varchar(200) null,
-	codigo_barras varchar(500),
-	imagem_base64 = varchar(500)
+	codigo_barras varchar,
+	imagem_base64 varchar
 	);
+	
+	create table sysgestao.tb_solicitacao_produto(
+	id_solicitacao serial not null primary key,
+	nome_destinatario varchar(200) not null,
+	status varchar(50) not null
+	);
+	
+	create table sysgestao.tb_item_solicitacao(
+	id_item serial not null primary key,
+	id_solicitacao integer not null,
+	id_produto integer not null,
+	quantidade integer not null,
+	constraint id_solicitacao_fk foreign key  (id_solicitacao)
+	references sysgestao.tb_solicitacao_produto(id_solicitacao),
+	constraint id_produto_fk foreign key  (id_produto)
+	references sysgestao.tb_produto(id_produto)
+	);
+	
+	
+	
+	
 
 	
